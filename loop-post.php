7343c7c3fdaefb7bd4af0d73ca13386fec31b9df
Yoeri Dekker <yoeri@csorbamedia.com>
2017-10-01 17:34:10 +0200

    Loop post fixes for grid and list layout

--- loop-post.php
+++ loop-post.php
@@ -1,30 +1,52 @@
 <?php 
 
 // Globals
-global $post, $i; 
+global $post; 
 
-// Define the archive inline styles
-$class = ( $i % 3 == 0 ) ? ' half' : '' ;
-if( $i === 0 ){
-    $class = ' half';
-}
-
+$class      = '';
+$layout     = (int) get_urnext_option('grid_layout') === 1 ? 'grid' : 'list' ;
 $type       = get_post_format();
-$image      = has_post_thumbnail() ? get_the_post_thumbnail_url() : false ;
+$imgagesize = $layout === 'grid' ? 'large' : 'medium' ;
+$image      = has_post_thumbnail() ? get_the_post_thumbnail_url( null, $imgagesize) : false ;
 $class     .= $image ? ' image' : ' no-image' ;
 $class     .= ' post-format-' . $type;
 $style      = $image ? sprintf('background-image:url(%s);', $image ) : '' ;
-$fittext    = ( $class === 'half' ) ? 'headadjust' : 'textadjust' ;
 ?>
-<!-- start grid item -->
-<div class="grid-item<?php echo $class; ?>" style="<?php echo $style; ?>">
-    <div class="inner header-text-color ">
-        <h3 class="<?php echo $fittext; ?> header-text-color "><?php the_title(); ?></h3>
-        <span class="date header-text-color textadjust"><?php the_time( get_option('date_format') ) ?></span>
-        <div class="centered header-text-color textadjust">
-            <a href="<?php the_permalink(); ?>" class="textadjust header-text-color btn center-button button small"><?php _e('Read more', 'urnext'); ?></a>
+
+<?php if( $layout === 'grid' ): ?>
+
+    <!-- start grid item -->
+    <article class="<?php echo urnext_grid_column_classes(); ?> border-page-color grid-item<?php echo $class; ?>" style="<?php echo $style; ?>">
+        <div class="inner primary-text-color">
+            <h3 class="primary-text-color"><?php the_title(); ?></h3>
+            <span class="date primary-text-color"><?php the_time( get_option('date_format') ) ?></span>
+            <?php if( (int) get_urnext_option('grid_show_excerpt') === 1 && has_excerpt() ): ?>
+                <?php the_excerpt(); ?>
+            <?php endif; ?>
+            <div class="centered primary-text-color">
+                <a href="<?php the_permalink(); ?>" class="primary-text-color btn center-button button small"><?php _e('Read more', 'urnext'); ?></a>
+            </div>
         </div>
-    </div>
-    <div class="corner bg-header-color"></div>
-</div>
-<!-- end grid item -->
+        <div class="corner bg-primary-color"></div>
+    </article>
+    <!-- end grid item -->
+
+<?php else: ?>
+
+    <!-- start list item -->
+    <article class="list-item<?php echo $class; ?>">
+        <div class="bg-primary-color slide-panel" style="<?php echo $style; ?>"></div>
+        <a href="<?php the_permalink(); ?>">
+            <h3 class="text-color"><?php the_title(); ?></h3>
+        </a>
+        <span class="date text-color textadjust"><?php the_time( get_option('date_format') ) ?></span>
+        <div class="text-color textadjust">
+            <a href="<?php the_permalink(); ?>" class="textadjust border-text-color text-color btn center-button button small">
+                <?php _e('Read more', 'urnext'); ?>
+            </a>
+        </div>
+    </article>
+    <div class="bordered border-text-color"></div>
+    <!-- end list item -->
+
+<?php endif; ?>
